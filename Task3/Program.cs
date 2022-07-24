4d0a131c255025ac02f75d497694a1ea8494f025
Basalyko Andrey <basalykoa.s@gmail.com>
2022-07-25 01:35:15 +0300

    Сделал третью  задачу.

---
+++ Task3/Program.cs
@@ -0,0 +1,48 @@
+﻿int [,] array = new int [3, 4];
+Random random = new Random ();
+int z = 0;
+int g = 0;
+double sum = 0;
+void method( int a, int b)
+{
+    while (a < 3)
+    {
+        sum = sum + array[a,b];
+        ++a;
+    }
+    double sch = sum / 3;
+    if (sch - Math.Round(sch) > 0.0)
+    {
+        Console.Write("{0,6:F2}",sch);
+        
+    }
+    else
+    {
+        if (Math.Round(sch) > sch)
+        {
+            Console.Write("{0,6:F2}",sch);
+        }
+        else
+        {
+            Console.Write(sch);
+        }
+    }
+}
+for (int i = 0; i < 3; i++)
+{ 
+    for (int j = 0; j < 4; j++)
+    { 
+        array[i,j] = random.Next(0,100);
+        Console.Write(array[i,j]);
+        Console.Write("; ");
+    }   
+    Console.WriteLine();
+}
+Console.Write("-> ");
+while (g < 4)
+{
+    method(z,g);
+    sum = 0;
+    Console.Write("; ");
+    ++g;
+}
