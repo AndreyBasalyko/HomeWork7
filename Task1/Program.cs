c819eb4f76ee08387c904fea86d11c7149d9f7e8
Basalyko Andrey <basalykoa.s@gmail.com>
2022-07-24 19:58:25 +0300

    Сделал первую задачу.

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+double [,] array = new double [m, n];
+Random random = new Random ();
+for (int i = 0; i < m; i++)
+{ 
+    for (int j = 0; j < n; j++)
+    { 
+        array[i,j] = random.Next(-100,100) / 10.0;
+        Console.Write(array[i,j]);
+        Console.Write("; ");
+    }   
+    Console.WriteLine();
+}
